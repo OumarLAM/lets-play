cf425cd28199261c2fcf1cc459a42b9fe33f101b
OumarLAM <oumarlam154@gmail.com>
2024-10-04 19:45:52 +0000

    Add lombok tools to the User collection

--- src/main/java/com/example/oulam/lets_play/model/User.java
+++ src/main/java/com/example/oulam/lets_play/model/User.java
@@ -1,6 +1,19 @@
 package com.example.oulam.lets_play.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
 public class User {
+    @Id
     private String id;
     private String name;
     private String email;
