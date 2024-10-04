e7372e6ef08a419a5104818d75c9e25528766864
OumarLAM <oumarlam154@gmail.com>
2024-10-04 03:11:06 +0000

    Generate a strong secret_key and define the User and Product models

---
+++ src/main/java/com/example/oulam/lets_play/model/Product.java
@@ -0,0 +1,9 @@
+package com.example.oulam.lets_play.model;
+
+public class Product {
+    private String id;
+    private String name;
+    private String description;
+    private Double price;
+    private String userId;
+}
---
+++ src/main/java/com/example/oulam/lets_play/model/User.java
@@ -0,0 +1,9 @@
+package com.example.oulam.lets_play.model;
+
+public class User {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+}
