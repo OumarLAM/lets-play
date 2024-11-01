7c0cce3fbf99fd1f8e5bc186260cb6ea9b8343b5
OumarLAM <oumarlam154@gmail.com>
2024-11-01 17:30:59 +0000

    Add authentication controller implementation

---
+++ src/main/java/com/example/oulam/lets_play/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.example.oulam.lets_play.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody LoginRequest request
+    ) {
+        return ResponseEntity.ok(service.login(request));
+    }
+}
